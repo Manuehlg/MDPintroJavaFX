dd388e9bc0f91136b35e4cc9a61881ddeee4d2bc
Manuel Lemus <151822942+Manuehlg@users.noreply.github.com>
2025-10-23 23:21:15 +0200

    prueba de boton con handler y validacion por catch exception

--- src/main/java/com/pp/demo/HelloApplication.java
+++ src/main/java/com/pp/demo/HelloApplication.java
@@ -3,10 +3,14 @@ package com.pp.demo;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,23 +18,59 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        StackPane root = new StackPane();
+        BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
 
+        //fijo el layouu
+        HBox arriba = new HBox();
+        root.setTop(arriba);
+        VBox abajo = new VBox();
+        root.setBottom(abajo);
+        HBox medio = new HBox();
+        root.setCenter(medio);
+        medio.setAlignment(Pos.CENTER);
+
+
+        TextField t = new TextField();
+        t.setText("te estoy hablandoooo");
+        medio.getChildren().add(t);
+
+        RadioButton botonraro = new RadioButton ("seleccioname");
         Button b = new Button("Prueba");
-
+        Button b2 = new Button("Prueba2");
+        Button b3 = new Button("Prueba3");
         // añade el componente al contenedor
-        root.getChildren().add(b);
+        arriba.getChildren().add(b);
+        abajo.getChildren().add(b2);
+        medio.getChildren().add(botonraro);
+        medio.getChildren().add(b3);
 
-        b.setText("Click me");
         b.setOnAction( new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event){
                 stage.setTitle("Nuevo Titulo");
             }
         });
-
+        b3.setOnAction( new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event){
+                t.setText("me han cambiadoooo");
+            }
+        });
+        b2.setOnAction( new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event){
+                try{
+                if ( Integer.parseInt(t.getText()) %2 == 0){
+                    t.setText("es par");
+                }
+                else if ( Integer.parseInt(t.getText()) %2 !=0){
+                    t.setText("es impar");
+                }}
+                catch (Exception e){
+                    t.setText("mete un numero chuleta?");
+                }
+            }
+        });
         stage.show();
     }
 
