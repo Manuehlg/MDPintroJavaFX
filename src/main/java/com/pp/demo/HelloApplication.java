0311961b12a5ee4dbdc209fe1de1ba47ceb2bd07
Manuel Lemus <151822942+Manuehlg@users.noreply.github.com>
2025-10-23 12:38:33 +0200

    Initial commit

---
+++ src/main/java/com/pp/demo/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.pp.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+
+        Button b = new Button("Prueba");
+        // añade el componente al contenedor
+        root.getChildren().add(b);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
